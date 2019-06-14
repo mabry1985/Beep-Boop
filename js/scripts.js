59c738a0d449c1f1de7ed0e8abc8eda3ebf9aba4
mabry1985 <mabry1985@gmail.com>
2019-06-14 09:43:56 -0700

    add logic to look for the number 2 while counting and replace it with Boop!

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function beepBoop(userNumber) {
   for (var index = 0; index < userNumber + 1; index++) {
     if (index === 3) {
-      return "I'm sorry, Dave. I'm afraid I can't do that."
+      console.log(`Im sorry, Dave. I'm afraid I can't do that.`);
+    } else if (index === 2) {
+      console.log('Boop!');
     } else {
-      console.log(index)
+      console.log(index);
     }
   }
 };
