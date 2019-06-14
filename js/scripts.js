4385ea4076b2dd469623cb59ee45851908adba2b
mabry1985 <mabry1985@gmail.com>
2019-06-14 09:33:27 -0700

    add logic to look for the number 3 while counting and replace it with Im sorry dave...

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 function beepBoop(userNumber) {
   for (var index = 0; index < userNumber + 1; index++) {
-    console.log(index)
+    if (index === 3) {
+      return "I'm sorry, Dave. I'm afraid I can't do that."
+    } else {
+      console.log(index)
+    }
   }
 };
