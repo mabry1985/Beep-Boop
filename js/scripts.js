0a588836580e65f0a6c79d784c0c5585514b3793
mabry1985 <mabry1985@gmail.com>
2019-06-14 19:56:07 -0700

    fixed easter egg

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,7 @@ function beepBoop(userNumber) {
   for (var i = 0; i < userNumber + 1; i++) {
     var numbers = i.toString().split(' ');
     numbers.forEach(function (number) {
-      if (parseInt(number) === 1980) {
-        $('.easter-egg').show();
-      }else if (number.indexOf('3') > -1) {
+      if (number.indexOf('3') > -1) {
         $('.output').append("<li> Im sorry, Dave. I'm afraid I can't do that.</li>");
       } else if (number.indexOf('2') > -1) {
         $('.output').append('<li> BOOP! </li>');
@@ -28,6 +26,10 @@ $(document).ready(function () {
     $('ul.output').empty();
     var userInput = parseInt($('#input').val());
     beepBoop(userInput);
+    if (userInput === 1980) {
+      $('.easter-egg').show();
+    };
+
     $('#input').val('');
   });
 });
