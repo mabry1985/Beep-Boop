31f7346b0f43f0a7d3c608d95e9bf82c8b556c35
mabry1985 <mabry1985@gmail.com>
2019-06-14 18:41:15 -0700

    add easter egg

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ function beepBoop(userNumber) {
   for (var i = 0; i < userNumber + 1; i++) {
     var numbers = i.toString().split(' ');
     numbers.forEach(function (number) {
-      if (number.indexOf('3') > -1) {
+      if (parseInt(number) === 1980) {
+        $('.easter-egg').show();
+      }else if (number.indexOf('3') > -1) {
         $('.output').append("<li> Im sorry, Dave. I'm afraid I can't do that.</li>");
       } else if (number.indexOf('2') > -1) {
         $('.output').append('<li> BOOP! </li>');
