24c9eb4f4cc28d026064e9ea775901b2e6f57362
mabry1985 <mabry1985@gmail.com>
2019-06-16 18:47:41 -0700

    add button and logic to take inputted number and count down to 0 and add form to take in user name and the logic to replace Dave on any number divisible by 3 by that user name

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,39 @@
 function beepBoop(userNumber) {
   for (var i = 0; i < userNumber + 1; i++) {
     var numbers = i.toString().split(' ');
+    var name = $('#name').val();
     numbers.forEach(function (number) {
-      if (number.indexOf('3') > -1) {
-        $('.output').append("<li> Im sorry, Dave. I'm afraid I can't do that.</li>");
+      if (i % 3 === 0) {
+        $('.output').append('<li> Im sorry, ' + name + ", I'm afraid I can't do that.</li>");
+      } else if (number.indexOf('3') > -1) {
+        $('.output').append("<li> Im sorry, Dave, I'm afraid I can't do that.</li>");
       } else if (number.indexOf('2') > -1) {
         $('.output').append('<li> BOOP! </li>');
       } else if (number.indexOf('1') > -1) {
         $('.output').append('<li> BEEP! </li>');
       } else {
         $('.output').append('<li>' + i + '</li>');
+        return;
+      }
+    });
+  };
+};
+
+function beepBoopReverse(userNumber) {
+  for (var i = 0; i < userNumber + 1; i++) {
+    var numbers = i.toString().split(' ');
+    var name = $('#name').val();
+    numbers.forEach(function (number) {
+      if (i % 3 === 0) {
+        $('.output').append('<li> Im sorry, ' + name + ", I'm afraid I can't do that.</li>");
+      } else if (number.indexOf('3') > -1) {
+        $('.output').append("<li> Im sorry, Dave, I'm afraid I can't do that.</li>");
+      } else if (number.indexOf('2') > -1) {
+        $('.output').prepend('<li> BOOP! </li>');
+      } else if (number.indexOf('1') > -1) {
+        $('.output').prepend('<li> BEEP! </li>');
+      } else {
+        $('.output').prepend('<li>' + i + '</li>');
         return;
       }
     });
@@ -26,11 +50,27 @@ $(document).ready(function () {
     $('ul.output').empty();
     $('.easter-egg').hide();
     var userInput = parseInt($('#input').val());
+    var name = $('#name').val();
     beepBoop(userInput);
     if (userInput === 1980) {
       $('.easter-egg').show();
     };
 
+    $('#name').val('');
+    $('#input').val('');
+  });
+
+  $('.reverse').click(function () {
+    $('ul.output').empty();
+    $('.easter-egg').hide();
+    var userInput = parseInt($('#input').val());
+    var name = $('#name').val();
+    beepBoopReverse(userInput);
+    if (userInput === 1980) {
+      $('.easter-egg').show();
+    };
+
+    $('#name').val('');
     $('#input').val('');
   });
 });
